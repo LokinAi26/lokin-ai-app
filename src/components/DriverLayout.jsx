261bd2aac935817c59c45cf00c66fe96ec8fbf6d
Kendall Hundley <kendallhundley95@gmail.com>
2026-08-14 15:44:03 +0000

    External agent changes

--- src/components/DriverLayout.jsx
+++ src/components/DriverLayout.jsx
@@ -7,6 +7,7 @@ import { base44 } from "@/api/base44Client";
 
 import QuickJumpRail from "@/components/QuickJumpRail";
 import CommandEngine from "@/components/CommandEngine";
+import GlobalVoiceAssistant from "@/components/GlobalVoiceAssistant";
 
 const NESTED_PATHS = [
   "/categories", "/locator", "/avoid", "/fuel", "/settings",
@@ -133,6 +134,7 @@ export default function DriverLayout() {
 
       {!lockedGps && <QuickJumpRail />}
       {!lockedGps && <CommandEngine open={cmdOpen} onClose={() => setCmdOpen(false)} />}
+      <GlobalVoiceAssistant />
     </div>
   );
 }
