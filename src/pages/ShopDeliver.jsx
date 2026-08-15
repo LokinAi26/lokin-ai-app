4e098ae01e08a542e1292f09cb6c11ddf38fbddc
Kendall Hundley <kendallhundley95@gmail.com>
2026-08-15 00:21:48 +0000

    External agent changes

--- src/pages/ShopDeliver.jsx
+++ src/pages/ShopDeliver.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useMemo, useState } from "react";
+import { Link } from "react-router-dom";
 import { ShoppingBag, Navigation, MapPin, Clock, DollarSign, ChevronRight, Package, RefreshCw, ScanLine } from "lucide-react";
 import { base44 } from "@/api/base44Client";
 import { CATEGORY_LABELS } from "@/lib/deliveryLabels";
