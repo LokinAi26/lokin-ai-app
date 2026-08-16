ea30a6c8a4296a5c26530a6abc19f5f0949bccc9
Kendall Hundley <kendallhundley95@gmail.com>
2026-08-16 22:17:32 +0000

    External agent changes

--- src/components/DriverLayout.jsx
+++ src/components/DriverLayout.jsx
@@ -11,7 +11,7 @@ import GlobalVoiceAssistant from "@/components/GlobalVoiceAssistant";
 
 const NESTED_PATHS = [
   "/categories", "/locator", "/avoid", "/fuel", "/settings",
-  "/drive", "/brand", "/support", "/gigs", "/receipts", "/pricing", "/on-the-road", "/shop-deliver",
+  "/drive", "/brand", "/support", "/gigs", "/receipts", "/pricing", "/on-the-road", "/shop-deliver", "/fitness",
 ];
 
 const TAB_ROOTS = {
